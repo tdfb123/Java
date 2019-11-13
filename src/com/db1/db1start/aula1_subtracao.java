16b96fc3254ff9d73f6c783e1bac4425be36fde5
Thiago <thiagofreitas1106@gmail.com>
2019-11-13 09:33:43 -0300

    Aula 1 - Exercício de subtracao

---
+++ src/com/db1/db1start/aula1_subtracao.java
@@ -0,0 +1,18 @@
+package com.db1.db1start;
+
+public class aula1_subtracao {
+
+    public static void main(String[] args) {
+
+        Integer numero1, numero2, resultado;
+
+        numero1 = 54;
+        numero2 = 45;
+
+        resultado = numero1 - numero2;
+
+        System.out.println("54 menos 45 é igual a " + resultado);
+
+    }
+
+}
