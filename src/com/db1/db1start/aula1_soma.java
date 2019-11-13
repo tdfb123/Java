500795c5675c6c44330ddfd436fb8fe0ca674f24
Thiago <thiagofreitas1106@gmail.com>
2019-11-13 09:27:40 -0300

    Aula 1 - Exercício de soma

---
+++ src/com/db1/db1start/aula1_soma.java
@@ -0,0 +1,17 @@
+package com.db1.db1start;
+public class aula1_soma {
+
+    public static void main(String[] args) {
+
+        Integer numero1, numero2, resultado;
+
+        numero1 = 54;
+        numero2 = 45;
+
+        resultado = numero1 + numero2;
+
+        System.out.println("A soma de 54 + 45 é igual a " + resultado);
+
+    }
+
+}
